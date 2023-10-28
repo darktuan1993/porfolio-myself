b4708d1be88f6e4975911e22dd922724828c84d2
DESKTOP-ALDKBOQ\admin <bachdangtuan93@gmail.com>
2023-10-29 01:06:40 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HeaderComponentComponent} from "./header-component/header-component.component";
+import {IntroComponentComponent} from "./intro-component/intro-component.component";
+import { CertComponentComponent } from './cert-component/cert-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    IntroComponentComponent,
+    CertComponentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/cert-component/cert-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cert-component',
+  templateUrl: './cert-component.component.html',
+  styleUrls: ['./cert-component.component.css']
+})
+export class CertComponentComponent {
+
+}
