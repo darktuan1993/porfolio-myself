1f81021e7c77d7fae7115bca529d2c3f92c4fa8e
DESKTOP-ALDKBOQ\admin <bachdangtuan93@gmail.com>
2023-10-29 01:51:06 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import {HeaderComponentComponent} from "./header-component/header-component.component";
 import {IntroComponentComponent} from "./intro-component/intro-component.component";
 import { CertComponentComponent } from './cert-component/cert-component.component';
+import { ProjectComponentComponent } from './project-component/project-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponentComponent,
     IntroComponentComponent,
-    CertComponentComponent
+    CertComponentComponent,
+    ProjectComponentComponent,
+    FooterComponentComponent
   ],
   imports: [
     BrowserModule
